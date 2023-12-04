c32d423549afd051f9c7c7e962df406f3eaa30f6
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-12-04 16:28:02 -0300

    novas rotas

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
   res.send('Bem-vindo à API de Avaliação da Educação Básica!');
 });
 
-app.get('/escolas', (req, res) => {
+app.get('/dados', (req, res) => {
   const { ano, idEscola, nomeEscola, tipoRede } = req.query;
 
   let sql = 'SELECT * FROM new WHERE 1';
@@ -71,8 +71,55 @@ app.get('/escolas', (req, res) => {
   });
 });
 
-// Substitua localhost pelo IP da sua máquina
-const ipAddress = '172.16.31.43'; // Substitua pelo seu IP
+app.get('/dados/comparacao', (req, res) => {
+  const { escola1, escola2, ano } = req.query;
+
+  if (!escola1 || !escola2 || !ano) {
+    res.status(400).json({ error: 'Parâmetros inválidos.' });
+    return;
+  }
+
+  let sql = `
+    SELECT id_escola, nome_escola, nota_media_padronizada as nota
+    FROM new
+    WHERE ano = ${ano}
+        AND (id_escola = ${escola1} OR id_escola = ${escola2})
+  `;
+
+  console.log('Consulta SQL:', sql);
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Erro ao executar a consulta:', err);
+      res.status(500).json({ error: 'Erro ao comparar as escolas.', details: err.message });
+    } else {
+      if (results.length < 2) {
+        res.json({ resultado: 'empate' });
+      } else {
+        const melhorEscola = results.reduce((melhor, escola) => {
+          return escola.nota > melhor.nota ? escola : melhor;
+        });
+
+        res.json({ resultado: melhorEscola });
+      }
+    }
+  });
+});
+
+// Rota modificada para incluir id_escola, nome_escola e nota_saeb_media_padronizada
+app.get('/dados/media-padronizada', (req, res) => {
+  connection.query('SELECT id_escola, nome_escola, nota_saeb_media_padronizada FROM new', (err, results) => {
+    if (err) {
+      console.error('Erro ao executar a consulta para Nota Média Padronizada:', err);
+      res.status(500).json({ error: 'Erro ao buscar a Nota Média Padronizada.', details: err.message });
+    } else {
+      console.log('Resultado da consulta para Nota Média Padronizada:', results);
+      res.json(results);
+    }
+  });
+});
+
+const ipAddress = '172.16.31.43';
 app.listen(port, ipAddress, () => {
   console.log(`Servidor rodando em http://${ipAddress}:${port}`);
 });
